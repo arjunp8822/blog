52c030f242904c0097f55053caebb8f54d6c4232
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2023-08-25 16:46:33 +1000

    added blog card and link

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import "../css/Home.css";
 
@@ -9,7 +10,6 @@ const Home = () => {
     const response = await axios.get("http://localhost:4000/blogs");
     const data = response.data;
     setBlogData(data);
-    console.log(data);
   };
 
   useEffect(() => {
@@ -17,16 +17,25 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
+    <div className="container">
       Test
-      {blogData.map((blog) => (
-        <article>
-          <h3>{blog.title}</h3>
-          <h5>{blog.description}</h5>
-          <img src={blog.img} />
-          <p className="content">{blog.content}</p>
-        </article>
-      ))}
+      <div className="blog-card-container">
+        {blogData.map((blog) => (
+          <article key={blog._id} className="blog-card">
+            <Link to={`/${blog._id}`}>
+              <img src={blog.img} />
+            </Link>
+            <div className="blog-card-main">
+              <span className="blog-card-category">JavaScript</span>
+              <span className="blog-card-date">19 January 2023</span>
+            </div>
+            <Link to={`/${blog._id}`}>
+              <h3>{blog.title}</h3>
+            </Link>
+            <h5>{blog.description}</h5>
+          </article>
+        ))}
+      </div>
     </div>
   );
 };
