0e5e9b3e762d450ef830c1accedc1b83db95327c
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2023-08-27 13:17:25 +1000

    added category and timestamps to mongodb model

--- backend/schemas/blogSchema.js
+++ backend/schemas/blogSchema.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  img: String,
-  content: String,
-});
+const blogSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    category: String,
+    img: String,
+    content: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const blogModel = mongoose.model("Blogs", blogSchema);
 
